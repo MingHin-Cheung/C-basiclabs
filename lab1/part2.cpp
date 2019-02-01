028f6403ccc6f5921847026cce5296374c487733
MingHin-Cheung <33229513+MingHin-Cheung@users.noreply.github.com>
2019-02-01 10:00:17 -0500

    Add files via upload

---
+++ lab1/part2.cpp
@@ -0,0 +1,11 @@
+//2. 
+//After saying "Hello ..." 
+//add "How are you?" 
+#include <iostream> 
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl;
+	cout << "How are you?" << endl;
+	return 0;
+}
---
+++ lab1/part3.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+
+int main() {
+	int num1, num2;
+	cout << "Enter first number: " << endl; 
+	cin >> num1; 
+	cout << "Enter second number: " << endl; 
+	cin >> num2;
+	//You do not need another variable to hold the sum 
+	//You can output(print) the value of an expression 
+	cout << "The sum of num1 and num2 is "  << num1 + num2; 
+	return 0;
+}
