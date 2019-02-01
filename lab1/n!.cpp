4d013b9f2d24e793aa2ea06bdacbfed295bd45aa
MingHin-Cheung <33229513+MingHin-Cheung@users.noreply.github.com>
2019-02-01 10:26:54 -0500

    Update n!.cpp

--- lab1/n!.cpp
+++ lab1/n!.cpp
@@ -6,9 +6,8 @@ using namespace std;
 int main(){
 	double nFactorial,n;
 	double pi= 3.14159;
-	double e = 2.71828;
 	cout << "Enter a number: ";
 	cin >> n;
-	nFactorial=sqrt(2*pi*n)*pow(n/e ,n);
+	nFactorial=sqrt(2*pi*n)*pow(n/exp(1) ,n);
 	cout << "The factorial of n is approximately " << nFactorial << endl;
 }
