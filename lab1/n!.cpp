da811f9db24bc3af55a9b3af366062eec5d01046
MingHin-Cheung <33229513+MingHin-Cheung@users.noreply.github.com>
2019-02-01 10:08:35 -0500

    Add files via upload

---
+++ lab1/n!.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+#include <cmath>
+using namespace std;
+
+int main(){
+	double nFactorial,n;
+	double pi= 3.14159;
+	double e = 2.71828;
+	cout << "Enter a number: ";
+	cin >> n;
+	nFactorial=sqrt(2*pi*n)*pow(n/e ,n);
+	cout << "The factorial of n is approximately " << nFactorial << endl;
+}
