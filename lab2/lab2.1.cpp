506019cb8aeed8d9da3c488b27bad6c6afdabcfc
MingHin-Cheung <33229513+MingHin-Cheung@users.noreply.github.com>
2019-02-08 10:03:31 -0500

    Create lab2.1.cpp

---
+++ lab2/lab2.1.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+using namespace std; 
+int main() {
+	int i = 5, 
+	j = 0, k, m; 
+	double f1 = 7.3, f2 = 0.32, f3 = 7.3e6, f4 = 1.2e6;
+	k = 4;
+	m = 7; 
+	cout << "3/2 = " << 3/2 << endl; //   3/2
+	cout << "3./2 = " << 3./2 << endl; //   3./2
+	cout << "7+4/2 = " << 7+4/2 << endl; //   7+4/2 
+	cout << "(7+4.)/2  = " << (7+4.)/2 << endl; //   (7+4.)/2  
+	cout << "7 % 2 = " << 7 % 2 << endl;//   7 % 2
+	cout << "24 % 2 = " << 24 % 2 << endl;//   24 % 2
+	cout << "k + m = " << k + m << endl;//   k + m 
+	cout << "f1 + k = " << f1 + k << endl;//   f1 + k 
+	cout << "f1 + f2 = " << f1 + f2 << endl;//   f1 + f2
+	cout << "f2 + f3 = " << f2 + f3 << endl;//   f2 + f3 
+	cout << "f3 + f4 = " << f3 + f4 << endl; //   f3 + f4 
+	return 0;
+
+} 
